55be2b886348b9dd32a6c3962ab15f813c148323
Molfar <sasha.prohor12@gmail.com>
2025-02-07 12:25:30 +0200

    Graceful shutdown was implemented

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"card-validator/internal/api"
 	"card-validator/internal/config"
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -32,5 +37,32 @@ func main() {
 
 	mux := app.Mount()
 
-	log.Fatal(app.Run(mux))
+	// Graceful Shutdown
+	server := http.Server{
+		Addr:    app.Config.Addr,
+		Handler: mux,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Print("Server Started")
+
+	<-done
+	log.Print("Server Stopped")
+
+	// shutdown context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// Gracefully shutdown
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	log.Print("Server Exited Properly")
 }
